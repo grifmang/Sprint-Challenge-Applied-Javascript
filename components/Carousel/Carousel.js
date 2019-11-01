75be4f4d11605e1cbab2f0f7b7a19b83db025123
Tim Griffith <grifmang@gmail.com>
2019-11-01 13:58:22 -0400

    Couldn't finish carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,55 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector('.carousel-container');
+const imageArray = [];
+const imageObject = axios.get('https://dog.ceo/api/breed/pug/images/random/12')
+  .then( (response) => {
+    imageArray.push(response.data.message)
+  })
+
+function createCarousel(imgArr) {
+  const carousel = document.createElement('div'),
+  leftButton = document.createElement('div'),
+  rightButton = document.createElement('div');
+
+  carousel.classList.add('carousel');
+  leftButton.classList.add('left-button');
+  rightButton.classList.add('right-button');
+
+  leftButton.textContent = '\u2190';
+  rightButton.textContent = '\u2192';
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(rightButton);
+
+  let carouselIndex = 1;
+  imageArray.forEach( (image) => {
+    const carouselImg = document.createElement('img');
+    carouselImg.src = image;
+    carousel.appendChild(carouselImg);
+  })
+
+  leftButton.addEventListener('click', () => {
+    const images = document.getElementsByTagName('.carousel-container img');
+    images.style.display('none');
+    if (carouselIndex > imageArray.length) {
+      carouselIndex %= imageArray.length;
+    }
+    else if (carouselIndex < 1) {
+      carouselIndex = 1;
+    }
+    else {
+      
+    }
+  })
+
+  rightButton.addEventListener('click', () => {
+
+  })
+
+  return carousel;
+}
+
+carouselContainer.appendChild(createCarousel(imageArray));
