20f2362a443b8435ad09d88ff25260380fc5df3d
Tim Griffith <grifmang@gmail.com>
2019-11-01 11:58:32 -0400

    Card Finished

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+const lambdaObject = axios.get("https://lambda-times-backend.herokuapp.com/articles").then( (response) => {
+    console.log(response.data);
+    // function to create cards.
+    Object.keys(response.data.articles).forEach( (key) => {
+        response.data.articles[key].forEach( (articleInfo) => {
+            cardContainer.appendChild(createCard(articleInfo.headline, articleInfo.authorPhoto, articleInfo.authorName));
+        })
+    })
+})
+
+function createCard(headline, photo, name) {
+    const card = document.createElement('div'),
+    cardHeadline = document.createElement('div'),
+    cardAuthor = document.createElement('div'),
+    cardImgDiv = document.createElement('div'),
+    cardImg = document.createElement('img');
+    cardSpan = document.createElement('span');
+
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    cardImgDiv.classList.add('img-container');
+
+    cardHeadline.textContent = headline;
+    cardSpan.textContent = `By ${name}`;
+    cardImg.src = photo;
+
+    cardAuthor.appendChild(cardSpan);
+    cardImgDiv.appendChild(cardImg);
+    cardAuthor.appendChild(cardImgDiv);
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+
+    return card;
+}
